d6cfe09d0a5bac8787eaf7c1c7c3d1abe6032557
STORYM <ssashkaa01@gmail.com>
2019-12-22 20:22:04 +0200

    add hw

---
+++ SortGame/SortGame/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using SortGame.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SortGame
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private SortModel sm = new SortModel();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            SortableNumbers.ItemsSource = sm.GetAllSortableNumbers();
+            TmpNumbers.ItemsSource = sm.GetAllTmpNumbers();
+
+        }
+
+        private void BtnToTmp_Click(object sender, RoutedEventArgs e)
+        {
+            sm.MoveToTmp();
+        }
+
+        private void BtnToSortable_Click(object sender, RoutedEventArgs e)
+        {
+            sm.MoveToSortable();
+
+            if(sm.CheckWin())
+            {
+                MessageBox.Show("YOU WIN!!!");
+                sm.Restart();
+            }
+        }
+
+        private void BtnRestart_Click(object sender, RoutedEventArgs e)
+        {
+            sm.Restart();
+        }
+    }
+}
---
+++ SortGame/SortGame/Models/SortModel.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortGame.Models
+{
+    class SortModel
+    {
+        private ObservableCollection<int> SortableNumbers = new ObservableCollection<int>();
+        private ObservableCollection<int> TmpNumbers = new ObservableCollection<int>();
+
+        private Random rnd = new Random();
+
+        // Максимальне число
+        private int max = 5;
+
+        public SortModel()
+        {
+            RegenerateMax();
+            GenerateNumbers();
+        }
+
+        // Перегенерувати максимальне число
+        private void RegenerateMax()
+        {
+            max = rnd.Next(3, 15);
+        }
+
+        // Згенерувати числа
+        private void GenerateNumbers()
+        {
+            int tmpNumber = rnd.Next(1, max);
+
+            for (int i = 0; i < max; i++)
+            {
+                while(!CheckUniqueNumber(tmpNumber))
+                {
+                     tmpNumber = rnd.Next(1, max + 1);
+                }
+             
+                SortableNumbers.Add(tmpNumber);
+            }
+        }
+
+        // Перевірити перемогу
+        public bool CheckWin()
+        {
+            if (SortableNumbers.Count < max) return false;
+
+            for(int i = 0; i < max; i++)
+            {
+                if (SortableNumbers.ElementAt(i) != i + 1)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public void Restart()
+        {
+            SortableNumbers.Clear();
+            TmpNumbers.Clear();
+            RegenerateMax();
+            GenerateNumbers();
+        }
+
+        // Перевірити існування числа в колекції
+        private bool CheckUniqueNumber(int number)
+        {
+            foreach(int n in SortableNumbers)
+            {
+                if(n == number)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        // Отримати список, що сортується
+        public ObservableCollection<int> GetAllSortableNumbers()
+        {
+            return SortableNumbers;
+        }
+
+        // Отримати тимчасовий список
+        public ObservableCollection<int> GetAllTmpNumbers()
+        {
+            return TmpNumbers;
+        }
+
+        // Перемістити число на тимчасове зберігання
+        public bool MoveToTmp()
+        {
+            if(SortableNumbers.Count() < 1)
+            {
+                return false;
+            }
+
+            TmpNumbers.Insert(0, SortableNumbers.First());
+
+            SortableNumbers.RemoveAt(0);
+
+            return true;
+        }
+
+        // Перемістити число 
+        public bool MoveToSortable()
+        {
+            if (TmpNumbers.Count() < 1)
+            {
+                return false;
+            }
+
+            SortableNumbers.Add(TmpNumbers.First());
+
+            TmpNumbers.RemoveAt(0);
+
+            return true;
+        }
+
+    }
+}
